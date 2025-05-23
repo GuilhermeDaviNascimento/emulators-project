21e82b9080f86295e6d76fd872a9f09893f3a778
Guilherme <guilherme.nascimento@nees.ufal.br>
2025-05-22 22:42:35 -0300

    feat: Carousel"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { CarouselOrientation } from "@/components/carousel";
+
+
 export default function Home() {
   return (
     <div className="">
+      <img src="/SNES_logo" alt="" />
+      <CarouselOrientation></CarouselOrientation>
     </div>
   );
 }
